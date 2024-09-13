55bf8b4cc782477f7426ee808a53a10bc8220ca5
vishiidevs <vspark007@gmail.com>
2024-09-13 19:58:32 +0530

    about,brands and footer added

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import Footer from './components/Footer';
 
 import "./styles/App.scss";
 import "./styles/Header.scss";
 import "./styles/Home.scss";
+import "./styles/Footer.scss";
 
 
 
@@ -15,6 +17,7 @@ export default function App() {
       <Routes>
           <Route path='/' element={<Home/>}/>
       </Routes>
+      <Footer/>
     </Router>
     
   )
--- myapp/src/components/Home.jsx
+++ myapp/src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import vg from '../assets/2.webp';
+import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillTwitterCircle} from 'react-icons/ai';
 
 export default function Home() {
   return (
     <>
-    <div className="home">
+    <div className="home" id='home'>
         <main>
             <h1>MyApp</h1>
             <p>Solution to all your problems</p>
@@ -16,6 +17,47 @@ export default function Home() {
             <p>
                 We are your one stop solution to the tech problems you face. We provide you with the best solutions to your problems. We have a team of experts who are always ready to help you.
             </p>
+        </div>
+    </div>
+    <div className="home3" id='about'>
+        <div>
+        <h1>Who we are</h1>
+        <p>
+            We are a team of experts who are always ready to help you. We provide you with the best solutions to your problems. We have a team of experts who are always ready to help you.
+        </p>
+        </div>
+    </div>
+    <div className="home4" id = 'brands'>
+        <div>
+        <h1>Brands</h1>
+         <article>
+            <div style={{
+                animationDelay:"0.3s",
+                }}>
+                <AiFillGoogleCircle/>
+                <p>Google</p>
+            </div>
+
+            <div style={{
+                animationDelay:"0.5s",
+                }}>
+                <AiFillAmazonCircle/>
+                <p>Amazon</p>
+            </div>
+
+            <div style={{
+                animationDelay:"0.7s",
+                }}>
+                <AiFillYoutube/>
+                <p>Youtube</p>
+            </div>
+            <div style={{
+                animationDelay:"0.9s",
+                }}>
+                <AiFillTwitterCircle/>
+                <p>Twitter</p>
+            </div>
+         </article>
         </div>
     </div>
     </>
