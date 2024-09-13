02785c88ac7079ba05c4995a15ba9088eac9b22b
vishiidevs <vspark007@gmail.com>
2024-09-13 22:03:44 +0530

    added services section and handled responsiveness

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,12 +3,14 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
+import Services from './components/Services';
 
 import "./styles/App.scss";
 import "./styles/Header.scss";
 import "./styles/Home.scss";
 import "./styles/Footer.scss";
 import "./styles/Contact.scss";
+import "./styles/mediaquery.scss";
 
 
 
@@ -19,6 +21,7 @@ export default function App() {
       <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/contact' element={<Contact/>}/>
+          <Route path='/services' element={<Services/>}/>
       </Routes>
       <Footer/>
     </Router>
