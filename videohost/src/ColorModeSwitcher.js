4c1cbb6c5df62cac37ccaa447a5a676fcccd1758
vishiidevs <vspark007@gmail.com>
2024-09-16 15:11:52 +0530

    initial commit

---
+++ videohost/src/ColorModeSwitcher.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+const ColorModeSwitcher = props => {
+  const { toggleColorMode } = useColorMode();
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+
+  return (
+    <IconButton
+      variant="ghost"
+      color="current"
+      pos={'fixed'}
+      top={'4'}
+      right={'4'}
+      zIndex={'overlay'}
+      onClick={toggleColorMode}
+      icon={<SwitchIcon />}
+      {...props}
+    />
+  );
+};
+
+export default ColorModeSwitcher;
+//in chakra ui 's personal unit system : 1 means = 4px
+//pos={'fixed'}
+// top={'4'}
+// right={'4'}
+// the above css sends the button to the top right corner of the screen
