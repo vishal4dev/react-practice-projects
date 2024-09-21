ce1f13afab99cc79e45edce0e5fc06e8c6764c6b
vishiidevs <vspark007@gmail.com>
2024-09-21 14:06:15 +0530

    moved files

--- crypto-flow/src/components/Coindetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Coindetails() {
-  return (
-    <div>Coindetails</div>
-  )
-}
-
-export default Coindetails
--- crypto-flow/src/components/Coins.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Coins() {
-  return (
-    <div>Coins</div>
-  )
-}
-
-export default Coins
